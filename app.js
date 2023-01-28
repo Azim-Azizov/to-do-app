8ec041ecaae60c8173c014c06ba08c90dc54ac7b
root <root@vmi896140.contaboserver.net>
2023-01-28 18:07:19 +0400

    adding mongo

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// jshint esversion:6
+
 const cron = require("node-cron");
+require("dotenv").config();
 
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
+const DB_URI = process.env.DB_URI;
 
 const options = {
   weekday: "long",
@@ -11,10 +16,18 @@ const options = {
   timeZone: "asia/baku",
 };
 
-const jobs = [];
 const banned = [];
 
 const app = express();
+
+mongoose.set("strictQuery", false);
+mongoose.connect(DB_URI, {useNewUrlParser: true});
+
+const jobSchema = new mongoose.Schema({
+  job: String
+});
+
+const Job = mongoose.model("Job", jobSchema);
 
 function isBanned(req, res, next) {
   const found = banned.find(obj => obj.ip == req.ip);
@@ -39,7 +52,13 @@ app.use(isBanned)
 
 app.get("/", (req, res) => {
   let today = new Date().toLocaleDateString("en-US", options);
-  res.render("index", { day: today, jobs: jobs });
+  Job.find((err, jobs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { day: today, jobs: jobs });
+    }
+  })
 })
 
 app.post("/", (req, res) => {
@@ -51,13 +70,18 @@ app.post("/", (req, res) => {
     banned.push({ "ip": req.ip, "date": new Date() });
     res.send('<script>alert("You are banned, because you used word php!");window.location = "/"</script>')
   } else {
-    jobs.push(req.body.job);
+    const job = new Job({
+      job: req.body.job
+    })
+    job.save();
     res.redirect("/");
   }
 })
 
 app.post("/delete", (req, res) => {
-  jobs.splice(Number(req.body.id), 1);
+  Job.deleteOne({_id: req.body.id}).then(() => {
+    console.log("Deleted");
+  });
   res.redirect("/");
 })
 
